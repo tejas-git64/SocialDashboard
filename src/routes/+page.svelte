6a75cce12a6f8b2db24b7b330ce181c2b1d5107b
tejas-git64 <tejas0586@gmail.com>
2023-10-12 18:42:29 +0530

    added chartjs

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,6 @@
-<script>
-
+<script lang="ts">
 </script>
 
 <div class="text-center">
-    <h1 class="text-3xl font-bold px-0 py-4 border-b-2">Dashboard in Svelte</h1>
+	<h1 class="text-3xl font-bold px-0 py-4 border-b-2">Dashboard in Svelte</h1>
 </div>
