bea7f6c0017044072b0861a51ad06eec80f4f4bb
tejas-git64 <tejas0586@gmail.com>
2023-10-14 15:33:47 +0530

    Moved dashboard to default page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
+	import WeeklyViews from "../components/Charts/WeeklyViews/WeeklyViews.svelte";
+
 </script>
 
-<div>
-    
+<div class="w-full h-full flex flex-col sm:flex-row items-start justify-start">
+	<div
+		class="w-full h-auto border-y-2 mx-auto flex-col md:flex-row items-center justify-start px-4 py-2"
+	>
+		<WeeklyViews />
+	</div>
 </div>
